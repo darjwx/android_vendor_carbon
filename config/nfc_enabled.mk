bce26339a4aef7c8037ebd6d73d7703034299584
Norris, Ryan N <ryan.norris@my.ccsu.edu>
2015-11-10 20:48:22 -0500

    hammerhead: Fix builds on the hammerhead.
    Change-Id: Iea72adcc1b444f209b48f2f3644ef48d55f73527

---
+++ config/nfc_enabled.mk
@@ -0,0 +1,3 @@
+# This device supports CarbonROM enhanced NFC
+PRODUCT_COPY_FILES += \
+    vendor/cm/config/permissions/com.carbon.nfc.enhanced.xml:system/etc/permissions/com.carbon.nfc.enhanced.xml
